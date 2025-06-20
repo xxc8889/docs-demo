303f75712a19e6d2ca2af25393122e3fff2f0fb7
xcxc <jiangzong811@gmail.com>
2025-06-21 02:09:16 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,64 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  base: "/docs-demo/",
+  head: [["link", { rel: "icon", href: "docs-demo/logo.jpg" }]],
+  title: "天成集团",
+  description: "A VitePress Site",
+  themeConfig: {
+    logo:'/logo.jpg',
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '首页', link:'/'},
+      { text: '介绍', link: '/markdown-examples' },
+      { text: '话术专栏', link: '/markdown-examples' },
+      { text: '关于', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: '初聊日程案例',
+        items: [
+          { text: '第一天初聊', link: '/markdown-examples' },
+          { text: '第一天话题', link: '/api-examples' }
+        ]
+      },
+      {
+        text: 'Examples',
+        items: [
+          { text: '话术专栏', link: '/markdown-examples' },
+          { text: 'Runtime API 话术专栏', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'telegram', link: 'https://t.me/tcyx888' }
+    ],
+    footer:{
+      copyright:"Copyright © 2019 天成集团"
+    },
+       // 设置搜索框的样式
+    search: {
+      provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索",
+            buttonAriaLabel: "搜索",
+          },
+          modal: {
+            noResultsText: "无法找到相关结果",
+            resetButtonTitle: "清除查询条件",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
+            },
+          },
+        },
+      },
+    },
+  }
+  
+})
---
+++ index.md
@@ -0,0 +1,35 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "天 成 集 团"
+  text: "代理 搭建 租赁"
+  tagline: 外汇 币盘 秒合约 商城 刷单 
+  image:
+   src: /bjt.png
+   alt: 背景图片
+  actions:
+    - theme: brand
+      text: 联系我们
+      link: https://t.me/tcyx888
+    - theme: alt
+      text: 查看盘口
+      link: /api-examples
+
+features:
+  - title: 简介
+    details: 天成集团是一家综合性的国际金融科技服务公司，专注于贵金属、股指、现货商品等金融衍生品系统在人工智能和智能大数据领域的规划咨询、软件研发实施、技术服务、系统集成及系统维护等
+  - title: 全球领先
+    details: 一直致力向客户提供自主研发的搭建服务。全方位交易解决方案，多年来追求卓越，不断推陈出新，提供创新尖端科技产品和专业服务
+  - title: 客服 维护
+    details: 天成集团拥有专业的技术团队，多名技术及客服24小时在线，平台的技术维护和服务质量业界首屈，一站式搭建为客户量身定制优质系统，指哪打哪
+  - title: 稳定
+    details: 服务器全海外部署专线传输，防爆红系统 平台不卡不滑，稳定、安全高效。
+  - title: 资金自管
+    details: 多年来，我们秉承信誉至上的理念。可提供电汇账户、当地账户 全球支付通道直通车。平台可挂地址，你收款再反点给我们
+  - title: 选择我们
+    details: 欢迎咨询，有问必答，24小时在线服务，已超500家合作客户
+---
+
+
